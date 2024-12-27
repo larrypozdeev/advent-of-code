b894e11bb8b57031db8d8d091e7f356f312bc22d
Larry Pozdeev <larrypozdeev@gmail.com>
2024-12-26 20:16:40 -0500

    init 2nd day

---
+++ 2024/day02/solution.go
@@ -0,0 +1,27 @@
+package day02
+
+import (
+	// "github.com/larrypozdeev/advent-of-code/2024/utils"
+)
+
+func Part1([][]int) int {
+	return 0
+}
+
+func Part2([][]int) {}
+
+func Run() {
+	// inputFilePath := utils.ReadInputFile("input.txt")
+	//
+	// inputLines, err := utils.ReadLines(inputFilePath)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// parsed := utils.ParseLines(inputLines)
+
+	// part1Result := Part1(parsed[0], parsed[1])
+	// part2Result := Part2(parsed[0], parsed[1])
+	//
+	// fmt.Println("Part 1 result:", part1Result)
+	// fmt.Println("Part 2 result:", part2Result)
+}
---
+++ 2024/day02/solution_test.go
@@ -0,0 +1,35 @@
+package day02
+
+import (
+	"testing"
+
+	"github.com/larrypozdeev/advent-of-code/2024/utils"
+)
+
+func TestPart1(t *testing.T) {
+	//7 6 4 2 1
+	// 1 2 7 8 9
+	// 9 7 6 2 1
+	// 1 3 2 4 5
+	// 8 6 4 4 1
+	// 1 3 6 7 9
+
+	data := []string{
+		"7 6 4 2 1",
+		"1 2 7 8 9",
+		"9 7 6 2 1",
+		"1 3 2 4 5",
+		"8 6 4 4 1",
+		"1 3 6 7 9",
+	}
+	parsed := utils.ParseLines(data)
+
+	expected := 11
+
+	if result := Part1(parsed); result != expected {
+		t.Errorf("Expected %d, but got %d", expected, result)
+	}
+}
+
+func TestPart2(t *testing.T) {
+}
